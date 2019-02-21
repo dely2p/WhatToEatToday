45fccded752cfd490149eb5b6bb43f1f856b34d8
dely2p <lhyal2p@gmail.com>
2019-02-21 18:19:20 +0900

    Add network-communication code to fetch menus and pull randomly

--- WhatToEatToday/WhatToEatToday/ResultViewController.swift
+++ WhatToEatToday/WhatToEatToday/ResultViewController.swift
@@ -7,11 +7,32 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultImageView: UIImageView!
+    
+    var arrayList = Array<String>()
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request("http://memolease.ipdisk.co.kr:1337/foods", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                for item in json.array! {
+                    self.arrayList.append(item["content"].stringValue)
+                }
+                self.resultLabel.text = self.arrayList.randomElement()!
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
 
     }
 
