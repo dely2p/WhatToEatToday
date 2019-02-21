0d1ab9be1df808f9c9b8b7cd56f7401647d9ac65
dely2p <lhyal2p@gmail.com>
2019-02-21 18:12:40 +0900

    Add ResultViewController to show random menu

---
+++ WhatToEatToday/WhatToEatToday/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  WhatToEatToday
+//
+//  Created by dely on 2019. 2. 21..
+//  Copyright © 2019년 dely. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
