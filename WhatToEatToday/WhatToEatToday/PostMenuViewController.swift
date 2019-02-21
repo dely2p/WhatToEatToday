fc190c9f9f03c75dd17e080292979144eb516536
dely2p <lhyal2p@gmail.com>
2019-02-21 20:31:56 +0900

    Add PostMenuViewController to make new menu

---
+++ WhatToEatToday/WhatToEatToday/PostMenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PostMenuViewController.swift
+//  WhatToEatToday
+//
+//  Created by dely on 2019. 2. 21..
+//  Copyright © 2019년 dely. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class PostMenuViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
